9e07ef30c9a6af139405d380a98adc734a8d2f9b
Alex Noel <116229513+anoel2@users.noreply.github.com>
2023-08-29 07:51:06 -0600

    Create check_megacli.py

---
+++ check_megacli.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+import argparse
+import subprocess
+
+#Exit Strings for informative output to Nagios/LibreNMS
+OK_STR = "The RAID is OK!"
+WARN_STR = "Warning! The RAID is Degraded!"
+CRIT_STR = "Critical! The RAID has FAILED!"
+UNK_STR = "Uh OH! Script is broken!"
+
+parser = argparse.ArgumentParser(description='Python script to check RAID status')
+parser.add_argument('-n', '--hostname', help='Hostname of the server', required=True)
+args = parser.parse_args()
+
+command = f"sudo /opt/MegaRAID/MegaCli/MegaCli64 -ShowSummary -a0"
+output = subprocess.check_output(f"{command}", shell=True).decode()
+
+if 'Degraded' in output:
+    print(WARN_STR)
+    exit(1)
+elif 'Failed' in output:
+    print(CRIT_STR)
+    exit(2)
+elif 'Online' in output:
+    print(OK_STR)
+    exit(0)
+else:
+    print(UNK_STR)
+    exit(3)
