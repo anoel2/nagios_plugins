9ab809e59a98d9f6ad96f2e843456db1460ee4fd
Alex Noel <116229513+anoel2@users.noreply.github.com>
2023-08-29 08:01:44 -0600

    added comments for use

--- check_megacli.py
+++ check_megacli.py
@@ -2,6 +2,7 @@
 import argparse
 import subprocess
 
+#This is to be ran over NRPE, with a service account on the distant end. 
 #Exit Strings for informative output to Nagios/LibreNMS
 OK_STR = "The RAID is OK!"
 WARN_STR = "Warning! The RAID is Degraded!"
@@ -11,6 +12,11 @@ UNK_STR = "Uh OH! Script is broken!"
 parser = argparse.ArgumentParser(description='Python script to check RAID status')
 parser.add_argument('-n', '--hostname', help='Hostname of the server', required=True)
 args = parser.parse_args()
+
+#In order for this to work, you will need a service account with permission to run this command.
+#I do not recommend giving root permissions to a service account. 
+#Instead, use visudo and provide the service account with permissions to run $command as root, and nothing else.
+
 
 command = f"sudo /opt/MegaRAID/MegaCli/MegaCli64 -ShowSummary -a0"
 output = subprocess.check_output(f"{command}", shell=True).decode()
